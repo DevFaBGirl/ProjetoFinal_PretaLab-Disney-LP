2143038247546090b13a917dd202d36881f3da08
DevFaBGirl <fabgirldev@gmail.com>
2023-03-14 23:02:03 -0300

    atualização do js media

--- js/media.js
+++ js/media.js
@@ -23,29 +23,17 @@ function calcularMedia() {
   const note4 = parseFloat(document.getElementById("nota4").value);
 
   let mensagem = "";
-  // evite mexer no código acima!
+  // evite mexer no código acima!git 
 
   //Calculando a média
   const media = (nota1 + nota2 + nota3 + nota4) / 4;
     
-  if (media === 0) {
-    mensagem = `Infelizmente você zerou a prova :(`;
-  } 
-  else if (media >= 0.1 && media <= 3) {
-    mensagem = `Caramba, deu ruim, você obteve média ${media}! Estude mais e tente novamente!`;
-  } 
-  else if (media >= 3.1 && media <= 5.9) {
-    mensagem = `Você obteve média ${media}! Falta pouco para a média.`;
-  } 
-  else if (media >= 6 && media <= 7) {
-    mensagem = `Você está na média com ${media}`;
-  } 
-  else if (media >= 7.1 && media <= 9.9) {
-    mensagem = `Notão! Sua média é ${media}!`;
-  } 
-  else if (media === 10) {
-    mensagem = `Hoje é seu aniversário? Pq você ta de parabéns! 10 de média`;
-  }
+  if (media === 0) {mensagem = "Infelizmente você zerou a prova :(";} 
+  else if (media >= 0.1 && media <= 3) {mensagem = "Caramba, deu ruim, você obteve média" + media + "! Estude mais e tente novamente!";} 
+  else if (media >= 3.1 && media <= 5.9) {mensagem = "Você obteve média" + media + " ! Falta pouco para a média.";} 
+  else if (media >= 6 && media <= 7) {mensagem = "Você está na média com" + media ;} 
+  else if (media >= 7.1 && media <= 9.9) {mensagem = "Notão! Sua média é" + media + "!";} 
+  else {mensagem = "Hoje é seu aniversário? Porque você ta de parabéns! 10 de média";}
 
   // evite mexer na linha de código abaixo!
   document.getElementById("situacaoAluno").innerHTML = mensagem;
